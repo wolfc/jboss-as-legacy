05fa9a64348163867ec6c02ca84cae2728dd655c
Emmanuel Hugonnet <ehugonne@redhat.com>
2013-11-08 17:55:11 +0100

    Wrapping the current NamingStore as a backup for looking for JNDI entries

--- deploy.sh
+++ deploy.sh
@@ -5,16 +5,15 @@ if [ "x$JBOSS_HOME" = "x" ]; then
     return 1
 fi
 mvn clean install
-cp -r lib/target/module/org $JBOSS_HOME/modules/system/layers/base/
-cp -r jnp/target/module/org $JBOSS_HOME/modules/system/layers/base/
-cp -r ejb3/target/module/org $JBOSS_HOME/modules/system/layers/base/
+cp -Rf lib/target/module/org $JBOSS_HOME/modules/system/layers/base/
+cp -Rf jnp/target/module/org $JBOSS_HOME/modules/system/layers/base/
+#cp -Rf ejb3/target/module/org $JBOSS_HOME/modules/system/layers/base/
 
 echo "Edit configuration file - for instance $JBOSS_HOME/standalone/configuration/standalone.xml"
 
 echo "To enable JNP:"
 echo "1 Add extension definition in <extensions>."
 echo "<extension module="org.jboss.legacy.jnp"/>"
-echo
 echo
 echo "2. Add sdubsystem definition(no args == default IP/port)"
 echo "<subsystem xmlns=\"urn:jboss:domain:legacy-jnp:1.0\">"
@@ -29,7 +28,7 @@ echo
 echo "<extension module="org.jboss.legacy.ejb3"/>"
 echo
 echo
-echo "2. Add sdubsystem definition(no args == default IP/port)"
+echo "2. Add subsystem definition(no args == default IP/port)"
 echo
 echo "<subsystem xmlns=\"urn:jboss:domain:legacy-ejb3:1.0\">"
 echo "    <remoting/>"
